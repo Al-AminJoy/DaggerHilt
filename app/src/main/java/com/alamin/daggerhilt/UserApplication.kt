0a460fdb90083e1fbba192657b170b55c38ad525
Al-AminJoy <alaminislam3555@gmail.com>
2022-04-24 12:44:53 +0600

    BrushUp

--- app/src/main/java/com/alamin/daggerhilt/UserApplication.kt
+++ app/src/main/java/com/alamin/daggerhilt/UserApplication.kt
@@ -2,12 +2,8 @@ package com.alamin.daggerhilt
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
 class UserApplication: Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-    }
 }
